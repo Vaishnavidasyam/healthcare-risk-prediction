402128298d060f1d752982999645dea1c5fb9afd
Vaishnavidasyam <vaishnavi.41469@gmail.com>
2026-07-09 13:36:52 +0530

    Update client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api", // backend URL
+  baseURL: 'https://healthcare-api-0x2n.onrender.com',
 });
 
 // Attach token if present
